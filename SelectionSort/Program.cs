6f26879b21f7c95e3433960cede857056de2c969
AdBirecki <abirecki@gmail.com>
2022-03-25 14:50:21 +0100

    Selection soert

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] array = { 1, 5, 7, 3, 4, 10, 22, 16, 2, -3, -18 };
+SelectionSort(array, array.Length - 1);
+
+Console.WriteLine(string.Join(',', array));
+
+void SelectionSort(int[] array, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j <= n; j++)
+        {
+            if (array[i] > array[j])
+            {
+                Swap(ref array[i], ref array[j]);
+            }
+        }
+    }
+}
+
+void Swap(ref int x, ref int y)
+{
+    (x, y) = (y, x);
+}
