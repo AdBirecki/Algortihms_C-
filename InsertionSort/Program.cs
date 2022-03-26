41c4ff918efee3fdd2da55443ff6e3890f7a96e5
AdBirecki <abirecki@gmail.com>
2022-03-26 21:35:21 +0100

    Insertion sort added|

---
+++ InsertionSort/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int[] arr = { 4, 12, 7, 14, 12, 66, 332, 4, 1, 3, 125 };
+InsertionSort(arr, arr.Length - 1);
+Console.WriteLine(string.Join(',', arr));
+
+void InsertionSort(int[] array, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j > 0; j--)
+        {
+            if (array[j - 1] > array[j])
+            {
+                Swap(ref array[j - 1], ref array[j]);
+            }
+        }
+        Console.WriteLine(string.Join(',', array));
+    }
+}
+
+
+void Swap(ref int x, ref int y)
+{
+    (x, y) = (y, x);
+}
